59483cf9eb503984ad459b9084dc67c2ad2b1053
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-06-12 05:27:37 +0530

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ app.use(cors(
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
-if(process.env.NODE_ENV==="production")
+if(process.env.NODE_ENV!=="production")
 {
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
     app.all('/{*any}',(req,res)=>
